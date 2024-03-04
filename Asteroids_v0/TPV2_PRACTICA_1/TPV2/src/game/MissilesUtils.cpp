6cca4b50004234d6445085d1b5571256cba0b4ad
VerXxuxZ <juserr04@ucm.es>
2024-03-04 18:21:57 +0100

    Fachadas

---
+++ Asteroids_v0/TPV2_PRACTICA_1/TPV2/src/game/MissilesUtils.cpp
@@ -0,0 +1,21 @@
+#include "MissilesUtils.h"
+
+#include "../components/Follow.h"
+#include "../components/Image.h"
+#include "../components/Transform.h"
+
+MissilesUtils::MissilesUtils()
+{
+}
+
+MissilesUtils::~MissilesUtils()
+{
+}
+
+void MissilesUtils::remove_all_missiles()
+{
+}
+
+void MissilesUtils::create_missiles(int n)
+{
+}

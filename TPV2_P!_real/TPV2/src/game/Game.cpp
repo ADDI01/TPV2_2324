dcec32b75787e6067d2456800fb95c480677664c
ADDI01 <dapala02@ucm.es>
2024-04-05 13:01:36 +0200

    Da error en el createBlackHoles

---
+++ TPV2_P!_real/TPV2/src/game/Game.cpp
@@ -0,0 +1,100 @@
+// This file is part of the course TPV2@UCM - Samir Genaim
+
+#include "Game.h"
+
+#include "../components/DeAcceleration.h"
+#include "../components/FighterCtrl.h"
+#include "../components/Image.h"
+#include "../components/ShowAtOpossiteSide.h"
+#include "../components/Transform.h"
+#include "../ecs/Manager.h"
+#include "../sdlutils/InputHandler.h"
+#include "../sdlutils/SDLUtils.h"
+#include "../utils/Vector2D.h"
+#include "../utils/Collisions.h"
+#include "AsteroidsUtils.h"
+#include "FighterUtils.h"
+#include "GameOverState.h"
+#include "NewGameState.h"
+#include "NewRoundState.h"
+#include "BlackHolesUtils.h"
+#include "MissilesUtils.h"
+
+#include "PausedState.h"
+#include "RunningState.h"
+
+using ecs::Manager;
+
+Game::Game() :
+		mngr_(new Manager()), //
+		ihdlr(ih()), //
+		current_state_(nullptr), //
+		paused_state_(nullptr), //
+		runing_state_(nullptr), //
+		newgame_state_(nullptr), //
+		newround_state_(nullptr), //
+		gameover_state_(nullptr) {
+
+}
+
+Game::~Game() {
+	delete mngr_;
+	delete ast_facede;
+	delete blackHoles_facede;
+	delete missiles_facede;
+	delete paused_state_;
+	delete runing_state_;
+	delete newgame_state_;
+	delete newround_state_;
+	delete gameover_state_;
+}
+
+void Game::init() {
+
+	// initialise the SDLUtils singleton
+	SDLUtils::init("ASTEROIDS", 800, 600,
+			"resources/config/asteroids.resources.json");
+
+	ast_facede = new AsteroidsUtils();
+	fighter_facede = new FighterUtils();
+	blackHoles_facede = new BlackHolesUtils();
+	missiles_facede = new MissilesUtils();
+
+	fighter_facede->create_fighter();
+
+	paused_state_ = new PausedState();
+	runing_state_ = new RunningState(ast_facede, fighter_facede, blackHoles_facede, missiles_facede);
+	newgame_state_ = new NewGameState(fighter_facede);
+	newround_state_ = new NewRoundState(ast_facede, fighter_facede,blackHoles_facede,missiles_facede);
+	gameover_state_ = new GameOverState();
+
+	current_state_ = newgame_state_;
+}
+
+void Game::start() {
+
+// a boolean to exit the loop
+	bool exit = false;
+
+	auto &ihdlr = ih();
+
+	while (!exit) {
+		Uint32 startTime = sdlutils().currRealTime();
+
+		// refresh the input handler
+		ihdlr.refresh();
+
+		if (ihdlr.isKeyDown(SDL_SCANCODE_ESCAPE)) {
+			exit = true;
+			continue;
+		}
+
+		current_state_->update();
+
+		Uint32 frameTime = sdlutils().currRealTime() - startTime;
+
+		if (frameTime < 10)
+			SDL_Delay(10 - frameTime);
+	}
+
+}
